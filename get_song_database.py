05e1e28b88f7e96414b2413f27ae0c41d24cd168
Yan233_ <Yan233th@126.com>
2023-08-04 15:13:14 +0800

    更换下载镜像

--- get_song_database.py
+++ get_song_database.py
@@ -3,12 +3,12 @@ import os
 
 def DownloadLatest ():
     print ('下载中...')
-    url = 'https://raw.githubusercontent.com/Arcaea-Infinity/ArcaeaSongDatabase/main/arcsong.db'
+    url = 'https://raw.gitmirror.com/Arcaea-Infinity/ArcaeaSongDatabase/main/arcsong.db'
     response = requests.get (url)
     open ('./arcsong.db', 'wb').write (response.content)
+    print ('下载完成')
 
 if __name__ == '__main__':
     if os.path.exists('./arcsong.db'):
         print ('"arcsong.db" 文件已存在, 将进行覆写')
     DownloadLatest ()
-    print ('完成')
